00fafaf2e439d852cefce1687bc0c1b4558aa3ad
tommyrharper <tommyrharper@gmail.com>
2020-03-31 17:51:35 +0100

    config.ru now working

--- app.rb
+++ app.rb
@@ -1,5 +1,10 @@
-require 'sinatra'
+require 'sinatra/base'
 
-get '/named-cat' do
-  "Hello Battle!"
+class Battle < Sinatra::Base
+  get '/' do
+    "Hello Battle!"
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
