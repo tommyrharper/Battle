3feb9196ee8e5f788c7c9f047a4077173fd6e252
Alex L <macbookpro@macbooks-mbp.home>
2020-04-01 17:30:37 +0100

    test - can see player 2 HP passes

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -10,7 +10,10 @@ end
 
 feature 'I want to see the other players hitpoints' do
   scenario "see player 2's hit points" do
-    visit('/play')
-  expect(page).to have_content "100HP"
+    visit('/')
+    fill_in :player_1_name, with: 'Dave'
+    fill_in :player_2_name, with: 'Mittens'
+    click_button 'Submit'
+  expect(page).to have_content "Mittens: 100HP"
   end
 end
