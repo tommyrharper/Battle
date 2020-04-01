972f51d7de2ea5d1887c1caa3362fb0c70ee83c4
Alex L <macbookpro@macbooks-mbp.home>
2020-04-01 14:52:15 +0100

    test failed - players fill in their names

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,3 +4,13 @@ feature 'Testing infrastructre' do
     expect(page).to have_content 'Testing infrastructure working!'
   end
 end
+
+feature 'players fill in their names' do
+  scenario 'enter names' do
+    visit('/')
+  fill_in :player_1_name, with: 'Dave'
+  fill_in :player_2_name, with: 'Mittens'
+  click_button 'Submit'
+  expect(page).to have_content 'Dave vs. Mittens'
+  end
+end
